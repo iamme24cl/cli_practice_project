28ee8d51d02dd29621a0f755090fea22e08d7552
Chandra Lama <iamme24cl@gmail.com>
2020-10-15 08:32:53 +0000

    just practicing

---
+++ lib/cli_project_practice.rb
@@ -0,0 +1,6 @@
+require "cli_project_practice/version"
+
+module CliProjectPractice
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cli_project_practice/version.rb
@@ -0,0 +1,3 @@
+module CliProjectPractice
+  VERSION = "0.1.0"
+end
